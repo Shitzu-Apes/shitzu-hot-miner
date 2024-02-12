7820334dc0c453f8d6f4c31d026fb7cf23d77bf3
Mario Reder <mario.reder@pm.me>
2024-02-12 10:21:28 +0100

    scaffold app via sveltekit

---
+++ app/.eslintrc.cjs
@@ -0,0 +1,19 @@
+/** @type { import("eslint").Linter.Config } */
+module.exports = {
+  root: true,
+  extends: ["plugin:svelte/recommended"],
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2020,
+    extraFileExtensions: [".svelte"],
+  },
+  overrides: [
+    {
+      files: ["*.svelte"],
+      parser: "svelte-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+      },
+    },
+  ],
+};
